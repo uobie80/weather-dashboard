ff25c363856c46665d614edccce0c2b78f57e5e3
Uchenna Obicheta <uobicheta@hotmail.com>
2023-03-13 11:09:20 -0400

    updated 5 day forecast.

--- script.js
+++ script.js
@@ -39,9 +39,9 @@ const weatherForecast = fetch(weatherForecastURL)
                                 });
 
         weatherForecast.then(function(data) {
-          
+          console.log(data.list)
          futureForecastDiv.innerHTML = "<div class=card-deck'><h5>5 Day Forecast</h5>" +  data.list.map((item, index) => { 
-              console.log(item)
+         
             
             let speed = item.wind.speed;
             let humidity = item.main.humidity;
@@ -100,7 +100,7 @@ const weatherForecast = fetch(weatherForecastURL)
                     </div>
                    </div>
                   `;
-                 } else if (index === 40){
+                 } else if (index === 34){
                     return `<div class="card mb-4 shadow-sm text-white bg-secondary future-weather-card" style="margin-right: 10px; width: 12rem; height: auto; float:left">
                     <img src="${weatherIconURL}" class="card-img-top" alt="weather icon" style="width: 5rem; height: 5rem;">
                        <div class="card-body">
